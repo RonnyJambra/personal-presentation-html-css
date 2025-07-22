1f6b66fadd97e23f81a30f8564e941c0d5e0e98a
Ronaldo Pereira <ronaldo.lp.oficial@gmail.com>
2025-07-21 21:11:18 -0300

    Ajusta borda suave da foto e melhora botões

---
+++ script.js
@@ -0,0 +1,8 @@
+function mostrarPortugues() {
+    document.querySelector('.apresentacao-pt').style.display = 'block';
+    document.querySelector('.apresentacao-en').style.display = 'none';
+}
+function mostrarIngles() {
+    document.querySelector('.apresentacao-pt').style.display = 'none';
+    document.querySelector('.apresentacao-en').style.display = 'block';
+}
